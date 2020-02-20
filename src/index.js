ad68b57d424253c330ff54981f7fbc41e8dc26d3
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-20 12:32:55 +0100

    feat: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+      return 0;
+  }
+  var SORT_ARR = array.sort(function(a, b) {
+      return a - b;
+  });
+  return SORT_ARR[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+      return 0;
+  }
+  var SORT_ARR = array.sort(function(a, b) {
+    return a - b;
+  });
+  return SORT_ARR[SORT_ARR.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+      return 0;
+  }
+  var SUM_ARR = 0;
+  array.forEach(function(item) {
+      SUM_ARR += item;
+  });
+  return SUM_ARR / array.length;
 }
